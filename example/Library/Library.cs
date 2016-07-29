a2c45f15131ab30bcd434f665c9146b83075fe1c
MiffyLiye <miffyliye@gmail.com>
2016-07-29 19:15:50 +0800

    Add usage example

---
+++ example/Library/Library.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+
+namespace ClassLibrary
+{
+    public class Library
+    {
+        private readonly TextReader _textReader;
+        private readonly TextWriter _textWriter;
+
+        public Library(TextReader textReader, TextWriter textWriter)
+        {
+            _textReader = textReader;
+            _textWriter = textWriter;
+        }
+
+        public void Run()
+        {    
+            _textWriter.WriteLine("Welcome!");
+            
+            var input = _textReader.ReadLine();
+            _textWriter.WriteLine(input);
+
+            _textReader.ReadLine();
+            _textWriter.WriteLine("Goodbye!");
+        }
+    }
+}
---
+++ example/LibraryTest/Tests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+using ClassLibrary;
+using MiffyLiye.XConsole;
+
+namespace Tests
+{
+    public class Tests
+    {
+        private string NewLine => Environment.NewLine;
+
+        [Fact]
+        public void test_reader_send_line_and_writer_output() 
+        {
+            var xReader = new XTextReader();
+            var xWriter = new XTextWriter();
+            var runner = new Library(xReader, xWriter);
+            var task = Task.Run(() => runner.Run());
+
+            Wait.Until(() => xReader.PendingRead);
+            
+            Assert.Equal(
+                "Welcome!" + NewLine,
+                xWriter.Output);
+            
+            var lastOutput = xWriter.Output;
+            var lastOutputLinesCount = xWriter.OutputLines.Count;
+
+            xReader.SendLine("hello");
+            Wait.Until(() => xWriter.OutputLines.Count > lastOutputLinesCount);
+            Wait.Until(() => xReader.PendingRead);
+            
+            Assert.Equal(
+                lastOutput 
+                + "hello" + NewLine, 
+                xWriter.Output);
+
+            lastOutput = xWriter.Output;
+            lastOutputLinesCount = xWriter.OutputLines.Count;
+
+            xReader.SendLine("");
+            task.Wait();
+
+            Assert.Equal(
+                lastOutput 
+                + "Goodbye!" + NewLine, 
+                xWriter.Output);            
+        }
+    }
+}
